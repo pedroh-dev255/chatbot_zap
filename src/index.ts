5e72c845e1611f04d7e249d19f3487d5350f5f87
Pedro Henrique <henriquepedro1912@gmail.com>
2022-06-09 10:43:12 -0300

    Post json, não recebe o message '-', error

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,12 @@ app.get('/status', (req: Request, res: Response)=>{
 })
 
 app.post('/send',async (req: Request, res: Response)=>{
+    const {number, message} = req.body
+    
     try{
-        await sender.sendText("5563992129216@c.us", "|Hoje tem cu?")
+        await sender.sendText(number, message)
+        
+        return res.status(200).json()
     } catch (error){
         console.log("error")
         res.status(500).json({status: "error", message: error})
